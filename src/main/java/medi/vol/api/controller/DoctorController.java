df725ddc732a7df4fbd082118ff9a42cf4d5e8ab
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-03 17:04:33 -0300

    feat(DoctorController): pass post params to class and create on MySQL

--- src/main/java/medi/vol/api/controller/DoctorController.java
+++ src/main/java/medi/vol/api/controller/DoctorController.java
@@ -1,20 +1,25 @@
 package medi.vol.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import medi.vol.api.doctor.Doctor;
 import medi.vol.api.doctor.DoctorRegistrationData;
+import medi.vol.api.doctor.DoctorRepository;
 
 @RestController
 @RequestMapping("doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DoctorRegistrationData data) {
-
-        System.out.println(data);
+        repository.save(new Doctor(data));
     }
 
 }
