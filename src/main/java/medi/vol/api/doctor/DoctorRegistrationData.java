acc262ac9f2c5f11f6812a852370a3c7d34eaa9a
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-03 17:03:58 -0300

    feat: add doctor class, interface and repository

--- src/main/java/medi/vol/api/doctor/DoctorRegistrationData.java
+++ src/main/java/medi/vol/api/doctor/DoctorRegistrationData.java
@@ -1,7 +1,7 @@
 package medi.vol.api.doctor;
 
-import medi.vol.api.adress.AdressData;
+import medi.vol.api.adress.Adress;
 
-public record DoctorRegistrationData(String name, String email, String crm, Specialty specialty, AdressData adress) {
+public record DoctorRegistrationData(String name, String email, String crm, Specialty specialty, Adress adress) {
 
 }
