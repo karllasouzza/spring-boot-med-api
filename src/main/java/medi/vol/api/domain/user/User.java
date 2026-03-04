b9f006e64ff2d84d9e350db64503829cf3fc7494
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-04 13:12:49 -0300

    feat(users): add user jpa interface and user table on migrations

---
+++ src/main/java/medi/vol/api/domain/user/User.java
@@ -0,0 +1,28 @@
+package medi.vol.api.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+}
