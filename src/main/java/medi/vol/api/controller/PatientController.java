f6f72d6a099597a78222d481e41ca65b56a431c6
Karlla Souzza <karlla.souzza7@gmail.com>
2026-03-04 09:46:40 -0300

    feat(patient): add returns and add a get patient by id method

--- src/main/java/medi/vol/api/controller/PatientController.java
+++ src/main/java/medi/vol/api/controller/PatientController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import medi.vol.api.patient.Patient;
+import medi.vol.api.patient.PatientExpandedPresenter;
 import medi.vol.api.patient.PatientListData;
 import medi.vol.api.patient.PatientRegistratioData;
 import medi.vol.api.patient.PatientRepository;
@@ -30,27 +33,46 @@ public class PatientController {
 
     @PostMapping
     @Transactional
-    public Patient registration(@RequestBody @Valid PatientRegistratioData data) {
-        Patient newPatient = repository.save(new Patient(data));
-        return newPatient;
+    public ResponseEntity registration(@RequestBody @Valid PatientRegistratioData data,
+            UriComponentsBuilder uriBuilder) {
+        Patient newPatient = new Patient(data);
+        repository.save(newPatient);
+        var uri = uriBuilder.path("/patients/{id}").buildAndExpand(newPatient.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new PatientExpandedPresenter(newPatient));
     }
 
     @GetMapping
-    public Page<PatientListData> list(@PageableDefault(size = 10, sort = { "name" }) Pageable pagination) {
-        return repository.findAllByDeletedAtNull(pagination).map(PatientListData::new);
+    public ResponseEntity<Page<PatientListData>> list(
+            @PageableDefault(size = 10, sort = { "name" }) Pageable pagination) {
+        var page = repository.findAllByDeletedAtNull(pagination).map(PatientListData::new);
+
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity getById(@PathVariable("id") Long id) {
+        Patient currentPatient = repository.getReferenceById(id);
+
+        return ResponseEntity.ok(new PatientExpandedPresenter(currentPatient));
     }
 
     @PutMapping
     @Transactional
-    public void update(@RequestBody @Valid UpdatePatientData data) {
+    public ResponseEntity update(@RequestBody @Valid UpdatePatientData data) {
         Patient currentPatient = repository.getReferenceById(data.id());
         currentPatient.updatePatientData(data);
+
+        return ResponseEntity.ok(new PatientExpandedPresenter(currentPatient));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable("id") Long id) {
+    public ResponseEntity delete(@PathVariable("id") Long id) {
         Patient currentPatient = repository.getReferenceById(id);
         currentPatient.softDelete();
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/medi/vol/api/patient/PatientExpandedPresenter.java
@@ -0,0 +1,12 @@
+package medi.vol.api.patient;
+
+import medi.vol.api.adress.Adress;
+
+public record PatientExpandedPresenter(
+        Long id, String name, String email, String Phone, String Cpf, Adress adress) {
+
+    public PatientExpandedPresenter(Patient patient) {
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getPhone(), patient.getCpf(),
+                patient.getAdress());
+    }
+}
